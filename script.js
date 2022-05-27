85e7fb58b03553eb9849b41f546469252c737a8d
Callum Hopkins <103919468@student.swin.edu.au>
2022-05-27 12:57:28 +1000

    Completed

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-let autoClicker = 1;
 let counter = 0;
 let clicks = 0;
+let interval = 1000;
+let autoClicker;
+let autoClickerSec = 0;
+let intervalDivisor = 1;
 
-setInterval(counters, clicks * 1000);
-setInterval(autoClickerLoop, 1000)
+setInterval(test, 250);
 
-function autoClickerLoop()
+function test()
+{
+    counters();
+    clicks++;
+    autoClickerIncrement();
+}
+
+function autoClickerUpdate()
+{
+    clearInterval(autoClicker);
+    autoClicker = setInterval(counters, interval);
+    if(autoClickerSec === 1)
+    {
+        document.getElementById("printAutoClicker").innerHTML = `${autoClickerSec} cookie per second`;  
+    } else
+    {
+        document.getElementById("printAutoClicker").innerHTML = `${autoClickerSec} cookies per second`;
+    }
+}
+
+function autoClickerIncrement()
 {
     if (clicks % 10 === 0 && clicks !== 0) 
     {
-        autoClicker *= 2;
+        autoClickerSec++;
+        interval = 1000 / intervalDivisor;
+        intervalDivisor++;
+        autoClickerUpdate();
     }
-    console.log(autoClicker);
+    console.log(interval); 
 }
 
 function counters() 
@@ -19,6 +44,3 @@ function counters()
     counter++;
     document.getElementById("counter").innerHTML = counter;
 }
-
-
-//console.log(`element == ${element}`);
